91d0f68478321f5bf96624ee3f2c4bf9d234defe
Ilowski <wojtek-ilowski@wp.pl>
2020-07-30 00:56:34 +0200

    Upload project with splash and login screen

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.util.Duration;
+
+import javax.swing.text.html.ImageView;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
---
+++ src/sample/LoginSreenController.java
@@ -0,0 +1,11 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginSreenController {
+
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
