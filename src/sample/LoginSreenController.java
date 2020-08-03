34517bec0146ca58812c8d9a20d969731ed719d9
= <wojtek-ilowski@wp.pl>
2020-08-03 16:04:45 +0200

    updated instance of DBConnection

--- src/sample/LoginSreenController.java
+++ src/sample/LoginSreenController.java
@@ -19,7 +19,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LoginSreenController {
-    DBConnection dbConnection = new DBConnection();
+    DBConnection dbConnection = DBConnection.getInstance();
     @FXML
     private TextField login;
     @FXML
