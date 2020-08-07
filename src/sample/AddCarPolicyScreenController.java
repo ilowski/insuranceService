83cfaf143ae4a75a3d15f226be9154c7affdfb98
= <wojtek-ilowski@wp.pl>
2020-08-07 19:00:21 +0200

    added dbConnection and updated method addPolicy

---
+++ src/sample/AddCarPolicyScreenController.java
@@ -0,0 +1,120 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class AddCarPolicyScreenController {
+        DBConnection dbConnection = DBConnection.getInstance();
+        @FXML
+        TextField nrPolicy;
+        @FXML
+        TextField name;
+        @FXML
+        TextField surname;
+        @FXML
+        TextField pesel;
+        @FXML
+        TextField address;
+        @FXML
+        ChoiceBox typePolicy;
+        @FXML
+        DatePicker startDatePolicy;
+        @FXML
+        DatePicker endDatePolicy;
+        @FXML
+        TextField registrationNr;
+        @FXML
+        TextField model;
+        @FXML
+        TextField mark;
+
+        public void initialize(URL url, ResourceBundle rb) {
+
+        }
+
+        @FXML
+        public void addPolicy(javafx.scene.input.MouseEvent mouseEvent) {
+            String queryStandard = "INSERT INTO " + dbConnection.tableNamePolicies + "(nrPolicy, name, surname, pesel, address, typePolicy, startDatePolicy, endDatePolicy) VALUES (?,?,?,?,?,?,?,?)";
+            String queryCars = "INSERT INTO " + dbConnection.tableNameCars + "(registrationNr, nrPolicy, mark, model) VALUES (?,?,?,?)";
+            PreparedStatement preparedStatement = null;
+
+            try {
+                preparedStatement = dbConnection.connection.prepareStatement(queryStandard);
+                preparedStatement.setString(1,nrPolicy.getText().toString().trim());
+                preparedStatement.setString(2,name.getText().toString().trim());
+                preparedStatement.setString(3,surname.getText().toString().trim());
+                preparedStatement.setString(4,pesel.getText().toString().trim());
+                preparedStatement.setString(5,address.getText().toString().trim());
+                preparedStatement.setString(6,typePolicy.getValue().toString().trim());
+                preparedStatement.setString(7,startDatePolicy.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                preparedStatement.setString(8,endDatePolicy.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                preparedStatement.execute();
+                preparedStatement = dbConnection.connection.prepareStatement(queryCars);
+                preparedStatement.setString(1,registrationNr.getText().toString().trim());
+                preparedStatement.setString(2,nrPolicy.getText().toString().trim());
+                preparedStatement.setString(3,mark.getText().toString().trim());
+                preparedStatement.setString(4,model.getText().toString().trim());
+                preparedStatement.execute();
+                preparedStatement.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                e.getMessage();
+            }
+
+        }
+
+        @FXML
+        public void getTypePolicy() {
+            if (typePolicy.getValue().toString().trim().equals("Samochodu")) {
+                Stage manageScreen = new Stage();
+                Parent root = null;
+                try {
+                    root = (Parent) FXMLLoader.load(getClass().getResource("AddCarPolicyScreen.fxml"));
+                } catch (Exception e) {
+                    Logger.getLogger(LoginSreenController.class.getName()).log(Level.SEVERE, null, e);
+                }
+                Stage current = (Stage) nrPolicy.getScene().getWindow();
+                Scene scene = new Scene(root, 1360, 730);
+                manageScreen.setScene(scene);
+                manageScreen.initStyle(StageStyle.TRANSPARENT);
+
+                current.hide();
+                manageScreen.show();
+            }
+        }
+        @FXML
+        public void returnMenu(javafx.scene.input.MouseEvent mouseEvent) {
+            Stage homeScreen = new Stage();
+            Parent root = null;
+            try {
+                root = (Parent) FXMLLoader.load(getClass().getResource("HomeScreen.fxml"));
+            } catch (Exception e) {
+                Logger.getLogger(LoginSreenController.class.getName()).log(Level.SEVERE, null, e);
+            }
+            Stage current = (Stage) nrPolicy.getScene().getWindow();
+            Scene scene = new Scene(root, 1360, 730);
+            homeScreen.setScene(scene);
+            homeScreen.initStyle(StageStyle.TRANSPARENT);
+
+            current.hide();
+            homeScreen.show();
+        }
+    }
+
+
